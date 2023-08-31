f40813676a660b873e1cf8d45347613e66a82135
Aiganym <llunareine@gmail.com>
2023-08-31 22:29:06 +0600

    update: update UserProfile

--- api/views.py
+++ api/views.py
@@ -43,18 +43,12 @@ class UserProfileView(generics.RetrieveUpdateDestroyAPIView):
     def get_object(self):
         return self.request.user
     def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        response = super().update(request, *args, **kwargs)
+        return response
     def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    def perform_destroy(self, instance):
-        instance.delete()
+        response = super().destroy(request, *args, **kwargs)
+        return response
+
 
 ############################ CHAT ######################################
 class ChatQueryView(generics.CreateAPIView):
