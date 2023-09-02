6757a10c33e363a69d5ba34e33461c279bbab19a
dinmukhammed <arystambekdimash005@gmail.com>
2023-09-02 22:47:07 +0600

    changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
-echo 'Running collecstatic...'
-python manage.py collectstatic --no-input --settings=config.settings.production
-
 echo 'Applying migrations...'
 python manage.py wait_for_db --settings=config.settings.production
 python manage.py migrate --settings=config.settings.production
 
 export DJANGO_SETTINGS_MODULE=bilim_ai.settings
-gunicorn bilim_ai.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn bilim_ai.wsgi:application --bind 0.0.0.0:8000
