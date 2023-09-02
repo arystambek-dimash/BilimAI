28bbf491f8bea7151c778b807ab74a76d4f57000
dinmukhammed <arystambekdimash005@gmail.com>
2023-09-03 05:02:43 +0600

    removed dotenv

--- bilim_ai/settings.py
+++ bilim_ai/settings.py
@@ -21,9 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = 'django-insecure-w5n9i5!84_x@+w7c#s!mztb%&1uk79$k(6&dmcq2ghz(shziin'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
