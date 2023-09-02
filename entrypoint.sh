f169852efb8fa94f487347129f1a65e9785e910e
dinmukhammed <arystambekdimash005@gmail.com>
2023-09-02 23:29:14 +0600

    changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@ export DJANGO_SETTINGS_MODULE=bilim_ai.settings
 echo 'Applying migrations...'
 python manage.py makemigrations
 python manage.py migrate
-ps aux | grep gunicorn
-kill -HUP PID
 
-gunicorn bilim_ai.wsgi:application --bind 0.0.0.0
+
+gunicorn bilim_ai.wsgi:application --bind 0.0.0.0:$PORT
