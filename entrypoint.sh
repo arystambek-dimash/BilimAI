0ee8f0109c7aaaf03f8005feda0412acb086e157
dinmukhammed <arystambekdimash005@gmail.com>
2023-09-02 23:25:09 +0600

    added kill gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,7 @@ export DJANGO_SETTINGS_MODULE=bilim_ai.settings
 echo 'Applying migrations...'
 python manage.py makemigrations
 python manage.py migrate
+ps aux | grep gunicorn
+kill -HUP PID
 
 gunicorn bilim_ai.wsgi:application --bind 0.0.0.0
