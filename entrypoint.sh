576c815b4d60f44d492b89642c2500362ad6ccf7
dinmukhammed <arystambekdimash005@gmail.com>
2023-09-02 23:08:43 +0600

    fixed urls entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ echo 'Applying migrations...'
 python manage.py makemigrations
 python manage.py migrate
 
-gunicorn bilim_ai.wsgi:application --bind 0.0.0.0:8000
+gunicorn bilim_ai.wsgi:application --bind 0.0.0.0
