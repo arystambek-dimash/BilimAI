81e7df4f062e69fdc6ea5677d721c2f085f071cc
dinmukhammed <arystambekdimash005@gmail.com>
2023-09-02 22:34:30 +0600

    changed to deploy

--- bilim_ai/settings.py
+++ bilim_ai/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "phonenumber_field",
+    'config',
 ]
 
 REST_FRAMEWORK = {
@@ -125,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
