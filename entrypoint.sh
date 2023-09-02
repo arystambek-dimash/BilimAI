09fb9450f76aa1da837fa4b8d3b4b525fe3c9cba
dinmukhammed <arystambekdimash005@gmail.com>
2023-09-03 00:16:54 +0600

    allowed_hosts changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 export DJANGO_SETTINGS_MODULE=bilim_ai.settings
-
+python manage.py collectstatic
 echo 'Applying migrations...'
 python manage.py makemigrations
 python manage.py migrate
-
 
 gunicorn bilim_ai.wsgi:application --bind 0.0.0.0
