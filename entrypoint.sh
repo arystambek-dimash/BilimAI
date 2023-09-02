9783351a4e94ace48db0f1cd4fab9e038fc7f15f
dinmukhammed <arystambekdimash005@gmail.com>
2023-09-02 23:00:16 +0600

    fixed urls entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
-echo 'Applying migrations...'
-python manage.py wait_for_db --settings=config.settings.production
-python manage.py migrate --settings=config.settings.production
-
 export DJANGO_SETTINGS_MODULE=bilim_ai.settings
+
+echo 'Applying migrations...'
+python manage.py wait_for_db
+python manage.py migrate
+
 gunicorn bilim_ai.wsgi:application --bind 0.0.0.0:8000
