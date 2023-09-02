e6ab95cf29c2d8d5e950d6a20641b9ae03c5e531
dinmukhammed <arystambekdimash005@gmail.com>
2023-09-02 22:41:02 +0600

    changed directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ echo 'Applying migrations...'
 python manage.py wait_for_db --settings=config.settings.production
 python manage.py migrate --settings=config.settings.production
 
-echo 'Running server...'
-gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application --bind 0.0.0.0:$PORT
+export DJANGO_SETTINGS_MODULE=bilim_ai.settings
+gunicorn bilim_ai.wsgi:application --bind 0.0.0.0:$PORT
