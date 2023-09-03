2f7acf13ec54fcae058eb455882db66e7a1adcad
dinmukhammed <arystambekdimash005@gmail.com>
2023-09-03 12:31:39 +0600

    changed #POST

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,4 @@ python manage.py collectstatic --noinput
 echo 'Applying migrations...'
 python manage.py migrate
 
-gunicorn bilim_ai.wsgi:application --bind 0.0.0.0:8000
-
-ls
+gunicorn bilim_ai.wsgi:application --bind 0.0.0.0:$PORT
