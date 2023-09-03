df4d0ec6ba885c60c21fdce8ec840e1caff429a0
dinmukhammed <arystambekdimash005@gmail.com>
2023-09-03 06:35:34 +0600

    updated

--- manage.py
+++ manage.py
@@ -7,6 +7,8 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bilim_ai.settings')
+    if not os.path.exists("static/"):
+        os.makedirs("static/")
     media_dir = "media/"
     if not os.path.exists(media_dir):
         os.makedirs(media_dir)
